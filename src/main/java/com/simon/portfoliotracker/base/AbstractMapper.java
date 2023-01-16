2929196b0da6446da039e5bc5ca42b180a716e2e
Simon <szymon.stypka@o2.pl>
2023-01-16 19:51:26 +0100

    feat: create abstract model mapper

--- src/main/java/com/simon/portfoliotracker/base/AbstractMapper.java
+++ src/main/java/com/simon/portfoliotracker/base/AbstractMapper.java
@@ -1,2 +1,18 @@
-package com.simon.portfoliotracker.base;public class AbstractMapper {
+package com.simon.portfoliotracker.base;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AbstractMapper<E, D>{
+
+    public abstract D mapToDto(E entity);
+    public abstract E mapToEntity(D dto);
+
+    public List<D> mapToDtos(List<E> sourceList){
+        return sourceList.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    public List<E> mapToEntities(List<D> dtoList){
+        return dtoList.stream().map(this::mapToEntity).collect(Collectors.toList());
+    }
 }
