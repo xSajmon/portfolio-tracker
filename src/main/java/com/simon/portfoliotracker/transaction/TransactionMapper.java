e37d783d3c0a6437e122a4d442983339a6d20574
Simon <szymon.stypka@o2.pl>
2023-01-16 20:51:06 +0100

    Add transaction mapper and move all related stuff from transaction service

---
+++ src/main/java/com/simon/portfoliotracker/transaction/TransactionMapper.java
@@ -0,0 +1,51 @@
+package com.simon.portfoliotracker.transaction;
+
+import com.simon.portfoliotracker.base.AbstractMapper;
+import com.simon.portfoliotracker.token.Token;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class TransactionMapper extends AbstractMapper<Transaction, TransactionRead> {
+    private final ModelMapper mapper;
+
+    public TransactionMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+        TypeMap<Transaction, TransactionRead> typeMap = mapper.createTypeMap(Transaction.class, TransactionRead.class);
+        typeMap.addMappings(typeMapper -> typeMapper.using(createTokenNameConverter()).map(Transaction::getToken, TransactionRead::setToken));
+        typeMap.addMappings(typeMapper -> typeMapper.using(createDateConverter()).map(Transaction::getDate, TransactionRead::setDate));
+    }
+    @Override
+    public TransactionRead mapToDto(Transaction entity) {
+        return mapper.map(entity, TransactionRead.class);
+    }
+
+    @Override
+    public Transaction mapToEntity(TransactionRead dto) {
+        return mapper.map(dto, Transaction.class);
+    }
+
+    private Converter<Token, String> createTokenNameConverter(){
+        return ctx -> generateFullTokenName(ctx.getSource().getName(), ctx.getSource().getSymbol());
+    }
+    private String generateFullTokenName(String name, String symbol){
+        if(!name.equals(symbol)){
+            return String.format("%s(%s)", name, symbol);
+        }
+        return name;
+    }
+
+    private Converter<LocalDateTime, String> createDateConverter(){
+        return ctx -> formatDate(ctx.getSource());
+    }
+    private String formatDate(LocalDateTime date){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
+        return date.format(format);
+    }
+
+}
--- src/main/java/com/simon/portfoliotracker/transaction/TransactionService.java
+++ src/main/java/com/simon/portfoliotracker/transaction/TransactionService.java
@@ -1,22 +1,12 @@
 package com.simon.portfoliotracker.transaction;
 
-import com.simon.portfoliotracker.ownedToken.OwnedToken;
-import com.simon.portfoliotracker.token.Token;
 import com.simon.portfoliotracker.token.TokenService;
-import com.simon.portfoliotracker.wallet.Wallet;
 import com.simon.portfoliotracker.wallet.WalletService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.Converter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,30 +15,10 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final WalletService walletService;
     private final TokenService tokenService;
-    private final ModelMapper mapper;
+    private final TransactionMapper mapper;
     private final ApplicationEventPublisher publisher;
 
-    @PostConstruct
-    private void postConstruct(){
-        Converter<Token, String> fullTokenName = mappingContext ->
-                generateFullTokenName(mappingContext.getSource().getName(), mappingContext.getSource().getSymbol());
 
-        Converter<LocalDateTime, String> date = mappingContext -> formatDate(mappingContext.getSource());
-        mapper.typeMap(Token.class, String.class).setConverter(fullTokenName);
-        mapper.typeMap(LocalDateTime.class, String.class).setConverter(date);
-    }
-
-    String generateFullTokenName(String name, String symbol){
-        if(!name.equals(symbol)){
-            return String.format("%s (%s)", name, symbol);
-        }
-        return name;
-    }
-
-    private String formatDate(LocalDateTime date){
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
-        return date.format(format);
-    }
 
     public Transaction buyTransaction(TransactionWrite transactionDto){
 
@@ -66,9 +36,7 @@ public class TransactionService {
     }
 
     public List<TransactionRead> getTransactions(){
-        return transactionRepository.findAll().stream()
-                .map(transaction -> mapper.map(transaction, TransactionRead.class))
-                .collect(Collectors.toList());
+        return mapper.mapToDtos(transactionRepository.findAll());
     }
 
 
